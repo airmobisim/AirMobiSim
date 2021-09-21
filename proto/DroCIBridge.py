cc5119c8d94fd37ca9fd028652ff76af39799fb2
Tobias Hardes <Tobias.Hardes@uni-paderborn.de>
2021-09-21 18:22:27 +0200

    Add new grpc method, restructuring file

--- proto/DroCIBridge.py
+++ proto/DroCIBridge.py
@@ -4,7 +4,6 @@ from concurrent import futures
 from src.simulationparameter import Simulationparameter
 
 import time
-#from src.repeatedtimer import Repeatedtimer
 from proto import airmobisim_pb2_grpc
 from proto import airmobisim_pb2
 
@@ -13,10 +12,20 @@ class AirMobiSim(airmobisim_pb2_grpc.AirMobiSimServicer):
 
     def __init__(self, simulation_obj):
         self._isRunning = False
+        self._isInitialized = True
         self.simulation_obj = simulation_obj
+        self._lastUavReport = []
+
+    def startSimulation(self):
+        self.simulation_obj.initializeNodes()
+        self._isRunning = True
 
     def Start(self, request, context):
-        pass    
+        pass
+
+    """
+        TODO:  ExecuteOneTimeStep should be an own method with the return GRPC statement 
+    """
 
     def ExecuteOneTimeStep(self, request, context):
         """
@@ -25,41 +34,58 @@ class AirMobiSim(airmobisim_pb2_grpc.AirMobiSimServicer):
         responseQuery = airmobisim_pb2.ResponseQuery()
 
         if not self._isRunning:
-
-            self.simulation_obj.initializeNodes()
-            self._isRunning = True 
+            self.startSimulation()
 
             for node in self.simulation_obj._managedNodes:
                 startPos = node._mobility._startPos
 
-                uav = airmobisim_pb2.Response(id=node._uid,x=startPos.x, y=startPos.y, z=startPos.z)
+                uav = airmobisim_pb2.Response(id=node._uid, x=startPos.x, y=startPos.y, z=startPos.z)
                 responseQuery.responses.append(uav)
 
             Simulationparameter.incrementCurrentSimStep()
 
             return responseQuery
         else:
-            #rt = Repeatedtimer(1, self.printStatus, "World")
+            # rt = Repeatedtimer(1, self.printStatus, "World")
 
             if Simulationparameter.currentSimStep < self.simulation_obj._simulationSteps:
+                self._lastUavReport = []
                 for node in self.simulation_obj._managedNodes:
                     node._mobility.makeMove()
+                    self._isInitialized = True
                     currentPos = node._mobility.getCurrentPos()
 
-                    uav = airmobisim_pb2.Response(id=node._uid,x=currentPos.x, y=currentPos.y, z=currentPos.z)
+                    uav = airmobisim_pb2.Response(id=node._uid, x=currentPos.x, y=currentPos.y, z=currentPos.z)
+                    self._lastUavReport.append(uav)
                     responseQuery.responses.append(uav)
 
-                Simulationparameter.incrementCurrentSimStep()    
+                Simulationparameter.incrementCurrentSimStep()
                 return responseQuery
 
-            else: 
-                #rt.stop()
+            else:
+                # rt.stop()
                 self.simulation_obj.finishSimulation()
-
-        currentNodes = self.simulation_obj.getNodes()
 
     def Finish(self, request, context):
         pass
+
+    def GetManagedHosts(self, request, context):
+        responseQuery = airmobisim_pb2.ResponseQuery()
+
+        if not self._isRunning:
+            self.startSimulation()
+
+        for node in self.simulation_obj._managedNodes:
+            if self._isInitialized:
+                node._mobility.makeMove()
+                self._isInitialized = True
+            currentPos = node._mobility.getCurrentPos()
+            uav = airmobisim_pb2.Response(id=node._uid, x=currentPos.x, y=currentPos.y, z=currentPos.z,
+                                          speed=1)  # TODO: Make speed a correct parameter
+            responseQuery.responses.append(uav)
+        return responseQuery
+
+
 
 
 def startServer(simulation_object):
@@ -80,4 +106,3 @@ def startServer(simulation_object):
     except KeyboardInterrupt:
         server.stop(0)
         print("Server has been stopped")
-
