bd46e514b7cd64b3e7807f6b9f9f6482c8da07cb
Tobias Hardes <Tobias.Hardes@uni-paderborn.de>
2021-09-21 18:20:59 +0200

    add more meaningful method names to grpc interface

--- proto/airmobisim_pb2_grpc.py
+++ proto/airmobisim_pb2_grpc.py
@@ -2,8 +2,8 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from proto import airmobisim_pb2 as airmobisim__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
+from proto import airmobisim_pb2 as proto_dot_airmobisim__pb2
 
 
 class AirMobiSimStub(object):
@@ -23,12 +23,17 @@ class AirMobiSimStub(object):
         self.ExecuteOneTimeStep = channel.unary_unary(
                 '/airmobisim.AirMobiSim/ExecuteOneTimeStep',
                 request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
-                response_deserializer=airmobisim__pb2.ResponseQuery.FromString,
+                response_deserializer=proto_dot_airmobisim__pb2.ResponseQuery.FromString,
                 )
         self.Finish = channel.unary_unary(
                 '/airmobisim.AirMobiSim/Finish',
                 request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
                 response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                )
+        self.GetManagedHosts = channel.unary_unary(
+                '/airmobisim.AirMobiSim/GetManagedHosts',
+                request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+                response_deserializer=proto_dot_airmobisim__pb2.UavList.FromString,
                 )
 
 
@@ -53,6 +58,12 @@ class AirMobiSimServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def GetManagedHosts(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_AirMobiSimServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -64,12 +75,17 @@ def add_AirMobiSimServicer_to_server(servicer, server):
             'ExecuteOneTimeStep': grpc.unary_unary_rpc_method_handler(
                     servicer.ExecuteOneTimeStep,
                     request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
-                    response_serializer=airmobisim__pb2.ResponseQuery.SerializeToString,
+                    response_serializer=proto_dot_airmobisim__pb2.ResponseQuery.SerializeToString,
             ),
             'Finish': grpc.unary_unary_rpc_method_handler(
                     servicer.Finish,
                     request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
                     response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+            ),
+            'GetManagedHosts': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetManagedHosts,
+                    request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                    response_serializer=proto_dot_airmobisim__pb2.UavList.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -111,7 +127,7 @@ class AirMobiSim(object):
             metadata=None):
         return grpc.experimental.unary_unary(request, target, '/airmobisim.AirMobiSim/ExecuteOneTimeStep',
             google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
-            airmobisim__pb2.ResponseQuery.FromString,
+            proto_dot_airmobisim__pb2.ResponseQuery.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
@@ -129,5 +145,22 @@ class AirMobiSim(object):
         return grpc.experimental.unary_unary(request, target, '/airmobisim.AirMobiSim/Finish',
             google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
             google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetManagedHosts(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/airmobisim.AirMobiSim/GetManagedHosts',
+            google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+            proto_dot_airmobisim__pb2.UavList.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
