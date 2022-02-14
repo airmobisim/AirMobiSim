eb31d73e583d49713805d07b1d7fd13d7041558a
Dalisha Logan <dalisha@campus.uni-paderborn.de>
2022-02-14 07:58:22 +0100

    broken pipe

--- airmobisim.py
+++ airmobisim.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 
 import sys
@@ -12,32 +12,47 @@ from src.yamlparser import Yamlparser
 
 from proto.DroCIBridge import startServer
 
+import os
+
+import errno
+
+import socket
+
+
 simulation: Simulation
 
-
 def main():
-    global simulation
-    parser = argparse.ArgumentParser(description='Importing configuration-file')
-    parser.add_argument('--configuration', action='store', type=str,
-                        default="examples/simpleSimulation/simulation.config", help='configuration')
-    parser.add_argument('--omnetpp', action='store_true', help='Start the OmNet++ simulator')
-    print(
-        """AirMobiSim Simulation  (C) 2021 Chair of Networked Systems Modelling TU Dresden.\nVersion: 0.0.1\nSee the license for distribution terms and warranty disclaimer""")
+    try:
+        global simulation
+        os.chdir("/home/dalisha.logan2/Documents/project_airmobisim/AirMobiSim")
+        parser = argparse.ArgumentParser(description='Importing configuration-file')
+        parser.add_argument('--configuration', action='store', type=str, default="examples/simpleSimulation/simulation.config", help='configuration')
+        parser.add_argument('--omnetpp', action='store_true', help='Start the OmNet++ simulator')
+        print("""AirMobiSim Simulation  (C) 2021 Chair of Networked Systems Modelling TU Dresden.\nVersion: 0.0.1\nSee the license for distribution terms and warranty disclaimer""")
 
-    args = parser.parse_args()
+        args = parser.parse_args()
+        p = Yamlparser(args.configuration)
+	
+        config = p.readConfig()
+        directory = pathlib.Path(args.configuration).parent.resolve()
+        initializeSimulation(config, directory)
 
-    p = Yamlparser(args.configuration)
-    config = p.readConfig()
-    directory = pathlib.Path(args.configuration).parent.resolve()
-    initializeSimulation(config, directory)
+        if args.omnetpp:
+           print("Start the AirMobiSim Server.....")
+           # Start the DroCI Bridge - Listen to OmNet++ incomes
+           startServer(simulation)
+        else:
+           print("Starting the Simulation")
+           simulation.startSimulation()
 
-    if args.omnetpp:
-        print("Start the AirMobiSim Server.....")
-        # Start the DroCI Bridge - Listen to OmNet++ incomes
-        startServer(simulation)
-    else:
-        print("Starting the simulation")
-        simulation.startSimulation()
+    except:
+           devnull = os.open(os.devnull, os.O_WRONLY)
+           #os.write(devnull, b'Hello')
+           os.dup2(devnull, sys.stdout.fileno())
+           sys.exit(1)
+
+ 
+        
 
 
 def initializeSimulation(config, directory):
