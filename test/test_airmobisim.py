e820ccf2ee4bb12a1176239dfd70cf558dfa832e
pritom5 <touhidhossainpritom5@gmail.com>
2022-03-02 10:52:29 +0100

    test speed limit

--- test/test_airmobisim.py
+++ test/test_airmobisim.py
@@ -1,46 +1,72 @@
 #!/usr/bin/env python3
 import unittest
 from src.yamlparser import Yamlparser
+from src.splinemobility import Splinemobility
 import airmobisim
 
 
 class TestAirmobisim(unittest.TestCase):
 
-    def test_input(self):
+    @classmethod
+    def setUpClass(cls) -> None:
         p = Yamlparser("../examples/simpleSimulation/simulation.config")
         config = p.readConfig()
         # print(config)
 
-        simTimeLimit= config['simulation']['simTimeLimit']
-        playgroundSizeX=config['simulation']['playgroundSizeX']
-        playgroundSizeY=config['simulation']['playgroundSizeY']
-        playgroundSizeZ=config['simulation']['playgroundSizeZ']
-        uavsSpline=config['uavsp']
+        cls.simTimeLimit = config['simulation']['simTimeLimit']
+        cls.playgroundSizeX = config['simulation']['playgroundSizeX']
+        cls.playgroundSizeY = config['simulation']['playgroundSizeY']
+        cls.playgroundSizeZ = config['simulation']['playgroundSizeZ']
+        cls.uavsSpline = config['uavsp']
+        cls.uavsLinear = config['uav']
 
-        speed = []
-        waypointX = []
-        waypointY = []
-        waypointZ = []
+        cls.speed = []
+        cls.waypointX = []
+        cls.waypointY = []
+        cls.waypointZ = []
 
-        for uavsp in config['uavsp']:
-            waypointX.append(uavsp['waypointX'])
-            waypointY.append(uavsp['waypointY'])
-            waypointZ.append(uavsp['waypointZ'])
-            speed.append(uavsp['speed'])
+        for uavsp in cls.uavsSpline:
+            cls.waypointX.append(uavsp['waypointX'])
+            cls.waypointY.append(uavsp['waypointY'])
+            cls.waypointZ.append(uavsp['waypointZ'])
+            cls.speed.append(uavsp['speed'])
 
+    @classmethod
+    def tearDownClass(cls) -> None:
+        #print('after everything')
+        pass
 
-        self.assertEqual(len(waypointX) == len(waypointY) == len(waypointZ), True)
+    def setUp(self) -> None:
+        #print('before running every test')
+        pass
 
-        for i,v in enumerate(waypointX):
+    def tearDown(self) -> None:
+        #print('after every test')
+        pass
+
+    def test_input_sp(self):
+
+        self.assertEqual(len(TestAirmobisim.waypointX) == len(TestAirmobisim.waypointY) == len(TestAirmobisim.waypointZ), True)
+
+        for i,v in enumerate(TestAirmobisim.waypointX):
             # validInputSp=all(0 <= item <=playgroundSizeX for item in v)
-            self.assertEqual(len(waypointX[i]) == len(waypointY[i]) == len(waypointZ[i]), True)
-            self.assertEqual(all(0 <= item <=playgroundSizeX for item in waypointX[i]), True)
-            self.assertEqual(all(0 <= item <=playgroundSizeY for item in waypointY[i]), True)
-            self.assertEqual(all(0 <= item <=playgroundSizeZ for item in waypointZ[i]), True)
+            self.assertEqual(len(TestAirmobisim.waypointX[i]) == len(TestAirmobisim.waypointY[i]) == len(TestAirmobisim.waypointZ[i]), True)
+            self.assertEqual(all(0 <= item <=TestAirmobisim.playgroundSizeX for item in TestAirmobisim.waypointX[i]), True)
+            self.assertEqual(all(0 <= item <=TestAirmobisim.playgroundSizeY for item in TestAirmobisim.waypointY[i]), True)
+            self.assertEqual(all(0 <= item <=TestAirmobisim.playgroundSizeZ for item in TestAirmobisim.waypointZ[i]), True)
+
+    def test_speed_limit_sp(self):
+        for index,uavsp in enumerate(TestAirmobisim.uavsSpline):
+
+            splineObj= Splinemobility(index,TestAirmobisim.speed[index],TestAirmobisim.waypointX[index],TestAirmobisim.waypointY[index],TestAirmobisim.waypointZ[index])
+            print('total flight time')
+            print(splineObj._totalFlightTime)
+            self.assertEqual(splineObj._totalFlightTime<=TestAirmobisim.simTimeLimit,True)
+        pass
 
 
 
 
-        # self.assertEqual(validInputSp,True)
+    # self.assertEqual(validInputSp,True)
 if __name__=='__main__':
     unittest.main()
