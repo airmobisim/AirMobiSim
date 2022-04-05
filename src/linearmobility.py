a0f93b69682c6def246f0cd277ecbe84e141cc4a
pritom5 <touhidhossainpritom5@gmail.com>
2022-04-05 15:16:20 +0200

    cleaning

--- src/linearmobility.py
+++ src/linearmobility.py
@@ -9,7 +9,7 @@ class Linearmobility(Basemobility):
     def __init__(self, uid, angle, startPos, endPos):
         super().__init__(uid, startPos, endPos)
         self._angle = angle
-        self._acceleration = 0 #acceleration not considered yet
+        self._acceleration = 0
         self._move.setStart(startPos, 0)
         self._move.setEndPos(endPos)
         self._move.setTempStartPos(startPos)
@@ -18,30 +18,12 @@ class Linearmobility(Basemobility):
         self._uid = uid
         self._stepTarget = ""
         self._totalFlightTime= self.computeTotalFlightTime()
+
     def makeMove(self):
         move = self.getMove()
-        passedTime = (
-                                 Simulationparameter.currentSimStep * Simulationparameter.stepLength) - self.getMove().getStartTime()
-        '''
-        print("----------------------------------------------------")
-        print("move.getSpeed(): "      + str(move.getSpeed()))
-        print("move.getStartPos().x: " + str(move.getStartPos().x))
-        print("move.getStartPos().y: " + str(move.getStartPos().y))
-        print("move.getStartPos().z: " + str(move.getStartPos().z))
-        print("angle: " + str(self._angle))
-        '''
-        # stepTargetX = move.getStartPos().x + move.getSpeed() * math.cos(math.pi * (self._angle/180)) * Simulationparameter.stepLength
-        # stepTargetY = move.getStartPos().y + move.getSpeed() * math.sin(math.pi * (self._angle/180)) * Simulationparameter.stepLength
-        ''' 
-        print("stepTargetX: " + str(stepTargetX))
-        print("stepTargetY: " + str(stepTargetY))
-        '''
-        # stepTarget = Point(stepTargetX, stepTargetY, self._startPos.z)
-        # self._stepTarget = stepTarget
-        # move.setDirectionByTarget(stepTarget)
+        passedTime = (Simulationparameter.currentSimStep * Simulationparameter.stepLength) - self.getMove().getStartTime()
 
         move.setDirectionByTarget()
-
         newSpeed = move.getSpeed() + self._acceleration * Simulationparameter.stepLength
 
         if passedTime>= self._totalFlightTime:
