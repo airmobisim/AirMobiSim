3ddf8eeb6b41e3dc93f24d343bd8cf5bf6b617ec
Simon Welzel <simon.welzel@uni-paderborn.de>
2022-06-13 11:24:59 +0200

    build.sh can now be run with -y flag that suppresses questions.

--- build.sh
+++ build.sh
@@ -21,11 +21,30 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+accept_all=false
+while [ "$1" != "" ]; do
+    case $1 in
+        -y | --non-interactive )          shift
+                                accept_all=true
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
 set -e 
 
 
 GRPC_VERSION=1.38.1
 PROTOC_VERSION=3.17.1
+
+
+
+
 base64 -d <<< "ICAgIF8gICAgXyAgICAgIF9fICBfXyAgICAgICBfICAgICBfIF9fX18gIF8KICAgLyBcICAoXylf
 IF9ffCAgXC8gIHwgX19fIHwgfF9fIChfKSBfX198KF8pXyBfXyBfX18KICAvIF8gXCB8IHwgJ19f
 fCB8XC98IHwvIF8gXHwgJ18gXHwgXF9fXyBcfCB8ICdfIGAgXyBcCiAvIF9fXyBcfCB8IHwgIHwg
@@ -42,9 +61,11 @@ echo "pyenv"
 
 echo "This setup installs all required Python packages (and poetry), loads the AirMobiSim extension from Veins, installs native binaries and libs from GRPC (version $GRPC_VERSION) and Protobuf ($PROTOC_VERSION) using conan.io.
 
-The complete source code is compiled afterwards.
-"
-read -p "Continue?" 
+The complete source code is compiled afterwards."
+if ! $accept_all
+then
+    read -p "Continue?" 
+fi
 
 
 ###################################
@@ -106,8 +127,17 @@ then
 	export PATH="$PYENV_ROOT/shims:$PATH"
 	eval "$(pyenv virtualenv-init -)"
 fi
-if !(pyenv install 3.9.0); then
-	echo "Python 3.9.0 will not be installed by this setup. Proceed with the setup.."
+
+if $accept_all
+then
+    pyenv install 3.9.0 -f
+fi
+
+if ! $accept_all
+then
+    if !(pyenv install 3.9.0); then
+        echo "Python 3.9.0 will not be installed by this setup. Proceed with the setup.."
+    fi
 fi
 eval "$(pyenv init -)"
 pyenv global 3.9.0
@@ -121,7 +151,7 @@ pyenv global 3.9.0
 ##################################
 if ! command -v poetry &> /dev/null
 then
-	echo "Installing poetry"
+    echo "Installing poetry"
 	if [[  "$OSTYPE" == "darwin"* ]]; then
 		brew install poetry
 	else
@@ -132,6 +162,7 @@ then
 	source $HOME/.poetry/env
 fi
 echo "Switching to python 3.9.0"
+
 mkdir -p $HOME/.cache/pypoetry/virtualenvs
 poetry env use 3.9.0
 ################################################################
@@ -142,8 +173,8 @@ poetry env use 3.9.0
 #|_|    \__, |\__|_| |_|\___/|_| |_| |____/ \___|\__|\__,_| .__/ 
 #       |___/                                             |_|    
 ################################################################
-echo "Installing required Python packages..."
 
+echo "Installing required Python packages..."
 
 poetry install
 poetry run python -m grpc_tools.protoc --python_out=. --grpc_python_out=. proto/airmobisim.proto -I .
@@ -185,13 +216,12 @@ if [  ! -f "$HOME/.conan/profiles/default" ]; then
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	echo "This is a Linux"
+    echo "This is a Linux"
 	poetry run bash -c "conan profile update settings.compiler.libcxx=libstdc++11 default"
 else
-	echo "This is a Mac"
+    echo "This is a Mac"
 	poetry run bash -c "conan profile update settings.compiler.version=13.0 default"
 fi
-
 
 echo "Starting installation of conan dependencies"
 
@@ -211,9 +241,9 @@ else
 	make -j$(nproc)
 fi
 
+echo "Successfully installed AirMobiSim!"
 
-echo "Everything worked"
-echo "Please run the following commands of add them to your .bashrc/.zshrc/..."
+echo "Please run the following commands or add them to your .bashrc/.zshrc/..."
 echo "-"
 echo "'export PATH="\$HOME/.poetry/bin:\$PATH"'"
 echo "'export AIRMOBISIMHOME=$AIRMOBISIMDIR'"
