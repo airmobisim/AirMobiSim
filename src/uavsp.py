fdf0c86e98b75f2e083f4aba2dcdc8746d79a57b
pritom5 <touhidhossainpritom5@gmail.com>
2022-06-21 13:13:17 +0200

    computing angle for spline mobility heading direction

--- src/uavsp.py
+++ src/uavsp.py
@@ -1,3 +1,4 @@
+import math
 from .basemobility import Basemobility
 from .splinemobility import Splinemobility
 
@@ -8,8 +9,21 @@ class UavSp:
 
     def __init__(self, uid,  waypointX, waypointY, waypointZ,speed,polygon_file_path,collision_action):
         self._uid = uid
+        self._angle = self.calculateAngle(waypointX, waypointY)
+        # print('angle: ',self._angle)
         self._mobility = Splinemobility(uid, waypointX, waypointY, waypointZ, speed, polygon_file_path,collision_action)
 
     # Should not be removed
     def getMobility(self):
         return self._mobility
+
+    def calculateAngle(self, waypointX, waypointY):
+        deltaY = waypointY[-1] - waypointY[0]
+        deltaX = waypointX[-1] - waypointX[0]
+
+        result = math.atan2(deltaY, deltaX)
+
+        if result < 0:
+            result = 360 + result
+
+        return result
