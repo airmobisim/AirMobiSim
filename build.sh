1e569982e01ed73ef1b0eed0d2bca57b32876a78
Tobias Hardes (local) <hardes-local@cms-lab06.cs.upb.de>
2024-06-24 09:23:39 +0200

    Fix conan version for installation

--- build.sh
+++ build.sh
@@ -174,7 +174,7 @@ poetry run python --version
 poetry install
 export PATH="$HOME/.poetry/bin:$PATH"
 
-pip3 install conan # We need a local installation outside poetry, since conan is required for the OMNeT++ part
+pip3 install conan==1.44.1 # We need a local installation outside poetry, since conan is required for the OMNeT++ part
 AIRMOBISIMDIR=$(pwd)
 
 export AIRMOBISIMHOME=$AIRMOBISIMDIR
